fac5a35c00806da5860fe8dd361a71f6355d9143
CBenjaminDP <benjamin.diaz@entrypay.net>
2025-05-26 17:44:37 -0600

    [FIRTS COMMIT] Subida Inicial Proyecto

---
+++ src/main/java/utez/edu/mx/cvcarlos9a/Cvcarlos9aApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvcarlos9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvcarlos9aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvcarlos9aApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/cvcarlos9a/Cvcarlos9aApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvcarlos9a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvcarlos9aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
